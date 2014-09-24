361e9d350fe902da2ab32789a5bc18ca18c37570
Nikola Petrov <nikolavp@gmail.com>
2014-09-24 10:29:51 +0300

    Cleanups and better installation instructions

--- init.lua
+++ init.lua
@@ -1,10 +1,27 @@
+-- Module dependencies
+local cairo = require("lgi").cairo
+local mouse = mouse
+local screen = screen
+local wibox = require('wibox')
+local table = table
+local timer = timer
+local keygrabber = keygrabber
+local math = require('math')
+local awful = require('awful')
+awful.client = require('awful.client')
+local gears_surface = require("gears.surface")
+
+local client = client
+
+module("alttab")
+
 --=========================== ALT-TAB ============================================
 local surface = cairo.ImageSurface(cairo.Format.RGB24,20,20)
 local cr = cairo.Context(surface)
 
 -- Create a wibox to contain all the client-widgets
 local preview_wbox = wibox({ bg = "#aaaaaaa0",
-			     width = screen[mouse.screen].geometry.width })
+                 width = screen[mouse.screen].geometry.width })
 
 preview_wbox.ontop = true
 preview_wbox.visible = false
@@ -51,29 +68,29 @@ function myAltTabPreview()
    for i = 1, #leftRightTab do
       preview_widgets[i] = wibox.widget.base.make_widget()
       preview_widgets[i].fit = function(preview_widget, width, height)
-   	 return preview_wbox.width / n, preview_wbox.height
+     return preview_wbox.width / n, preview_wbox.height
       end
-      
+
       preview_widgets[i].draw = function(preview_widget, preview_wbox, cr, width, height)
-   	 if width ~= 0 and height ~= 0 then
-	    
-   	    local c = leftRightTab[i]
-   	    local cg = c:geometry()
-   	    local sx = width / cg.width
-   	    local sy = height / cg.height
+     if width ~= 0 and height ~= 0 then
 
-	    local a = 0.7
-	    if c == altTabTable[altTabIndex] then
-	       a = 0.9
-	    end
-	    sx = sx * a
-	    sy = sy * a
+        local c = leftRightTab[i]
+        local cg = c:geometry()
+        local sx = width / cg.width
+        local sy = height / cg.height
 
-	    cr:translate((1-a)/2 * width, (1-a)/2 * height)
-	    cr:scale(sx, sy)
-	    cr:set_source_surface(require("gears.surface")(c.content), 0, 0)
-	    cr:paint()
-   	 end
+        local a = 0.7
+        if c == altTabTable[altTabIndex] then
+           a = 0.9
+        end
+        sx = sx * a
+        sy = sy * a
+
+        cr:translate((1-a)/2 * width, (1-a)/2 * height)
+        cr:scale(sx, sy)
+        cr:set_source_surface(gears_surface(c.content), 0, 0)
+        cr:paint()
+     end
       end
 
       preview_live_timer:connect_signal("timeout", function() preview_widgets[i]:emit_signal("widget::updated") end)
@@ -88,7 +105,7 @@ function myAltTabPreview()
 
    --layout
    preview_layout = wibox.layout.fixed.horizontal()
-   
+
    preview_layout:add(spacer)
    for i = 1, #leftRightTab do
       preview_layout:add(preview_widgets[i])
@@ -112,7 +129,7 @@ function myAltTabCycle(altTabTable, altTabIndex, altTabMinimized, dir)
    return altTabIndex
 end
 
-function myAltTab(dir, alt, tab, shift_tab)
+return function (dir, alt, tab, shift_tab)
 
    altTabTable = {}
    local altTabMinimized = {}
@@ -145,28 +162,28 @@ function myAltTab(dir, alt, tab, shift_tab)
       -- check if the client is on the current tag
       local isCurrentTag = false
       for j = 1, #ctags do
-	 if t == ctags[j] then
-	    isCurrentTag = true
-	    break
-	 end
+     if t == ctags[j] then
+        isCurrentTag = true
+        break
+     end
       end
 
       if isCurrentTag then
-	 -- check if client is already in the history
-	 -- if not, add it
-	 local addToTable = true
-	 for k = 1, #altTabTable do
-	    if altTabTable[k] == c then
-	       addToTable = false
-	       break
-	    end
-	 end
+     -- check if client is already in the history
+     -- if not, add it
+     local addToTable = true
+     for k = 1, #altTabTable do
+        if altTabTable[k] == c then
+           addToTable = false
+           break
+        end
+     end
 
 
-	 if addToTable then
-	    table.insert(altTabTable, c)
-	    table.insert(altTabMinimized, c.minimized)
-	 end
+     if addToTable then
+        table.insert(altTabTable, c)
+        table.insert(altTabMinimized, c.minimized)
+     end
       end
    end
 
@@ -184,9 +201,9 @@ function myAltTab(dir, alt, tab, shift_tab)
    local previewDelay = 0.1
    local previewDelayTimer = timer({timeout = previewDelay})
    previewDelayTimer:connect_signal("timeout", function() 
-				          preview_wbox.visible = true
-					  previewDelayTimer:stop()
-					  myAltTabPreview(altTabTable, altTabIndex) 
+                          preview_wbox.visible = true
+                      previewDelayTimer:stop()
+                      myAltTabPreview(altTabTable, altTabIndex) 
    end)
    previewDelayTimer:start()
 
@@ -194,33 +211,32 @@ function myAltTab(dir, alt, tab, shift_tab)
    -- as long as the user is alt-tabbing:
    keygrabber.run(
       function (mod, key, event)  
-	 -- Stop alt-tabbing when the alt-key is released
-	 if key == alt and event == "release" then
-	    
-	    local c
-	    for i = 1, altTabIndex - 1 do
-	       c = altTabTable[altTabIndex - i]
-	       c:raise()
-	       client.focus = c
-	    end
+     -- Stop alt-tabbing when the alt-key is released
+     if key == alt and event == "release" then
+        local c
+        for i = 1, altTabIndex - 1 do
+           c = altTabTable[altTabIndex - i]
+           c:raise()
+           client.focus = c
+        end
 
-	    c = altTabTable[altTabIndex]
-	    c:raise()
-	    client.focus = c                  
+        c = altTabTable[altTabIndex]
+        c:raise()
+        client.focus = c
 
-	    preview_wbox.visible = false
-	    previewDelayTimer:stop()
-	    preview_live_timer:stop()
-	    keygrabber.stop()
+        preview_wbox.visible = false
+        previewDelayTimer:stop()
+        preview_live_timer:stop()
+        keygrabber.stop()
 
-      	    -- Move to next client on each Tab-press
-	 elseif key == tab and event == "press" then
-	    altTabIndex = myAltTabCycle(altTabTable, altTabIndex, altTabMinimized, 1)
+            -- Move to next client on each Tab-press
+     elseif key == tab and event == "press" then
+        altTabIndex = myAltTabCycle(altTabTable, altTabIndex, altTabMinimized, 1)
 
-      	    -- Move to previous client on Shift-Tab
-	 elseif key == shift_tab and event == "press" then
-	    altTabIndex = myAltTabCycle(altTabTable, altTabIndex, altTabMinimized, -1)
-	 end
+            -- Move to previous client on Shift-Tab
+     elseif key == shift_tab and event == "press" then
+        altTabIndex = myAltTabCycle(altTabTable, altTabIndex, altTabMinimized, -1)
+     end
       end
    )
 
@@ -228,18 +244,3 @@ function myAltTab(dir, alt, tab, shift_tab)
    altTabIndex = myAltTabCycle(altTabTable, altTabIndex, altTabMinimized, dir)
 
 end -- function myAltTab
-
---========================= / ALT-TAB =====================================
-
--- Add this to your key-bindings table:
-    awful.key({ "Mod1",           }, "Tab",
-        function ()
-    	   myAltTab(1, "Alt_L", "Tab", "ISO_Left_Tab")
-        end
-    ),
-
-    awful.key({ "Mod1", "Shift"   }, "Tab",
-        function ()
-    	   myAltTab(-1, "Alt_L", "Tab", "ISO_Left_Tab")
-        end
-    ),
